216fb56cc03598342164727069835747e1abd76f
Abhay Kashyap <kashyaa2@union.edu>
2025-07-27 13:11:19 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     CORS(app)
     
     # Register blueprints
-    app.register_blueprint(routes_bp)
+    app.register_blueprint(routes_bp, url_prefix='/api)
 
     
     return app
